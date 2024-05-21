abefb35088db944f8ad38819d3116b585ad58b2f
Maxwell MacAvoy <maxwellmacavoy@Maxwells-MacBook-Air.local>
2024-05-21 19:53:01 -0400

    latest commit

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,13 +1,15 @@
-//Maxwell MacAvoy
+// Maxwell MacAvoy
 import Lab1 from "./Lab1";
 import { Route, Routes, Navigate } from "react-router";
 import TOC from "./TOC";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
+
 export default function Labs() {
   return (
     <div>
       <h1>Labs</h1>
+      <h3> Maxwell MacAvoy </h3>
       <TOC />
       <Routes>
         <Route path="/" element={<Navigate to="Lab1" />} />
@@ -15,6 +17,13 @@ export default function Labs() {
         <Route path="Lab2" element={<Lab2 />} />
         <Route path="Lab3" element={<Lab3 />} />
       </Routes>
+      <footer>
+        <p>
+          <a href="https://github.com/clfford00/kanbas-react-web-app" target="_blank" rel="noopener noreferrer">
+            My GitHub
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
