aa2b5a2884f7d7d33b024f2824aaad6a51fecccc
Maxwell MacAvoy <maxwellmacavoy@Maxwells-MacBook-Air.local>
2024-06-19 14:02:15 +0200

    second push to a3 to save work

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -1,67 +1,43 @@
-import ModuleControls from "./ModuleControls";
+import { useParams } from "react-router";
+import * as db from "../../Database";
 import { BsGripVertical } from "react-icons/bs";
+import ModuleControls from "./ModuleControls";
 import ModuleControlButtons from "./ModuleControlButtons";
 import LessonControlButtons from "./LessonControlButtons";
-
+//import "./styles.css"
 export default function Modules() {
-  return (
-    <div>
-      <ModuleControls /><br /><br /><br />
-      <ul id="wd-modules" className="list-group rounded-0">
-        <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
-          <div className="wd-title p-3 ps-2 bg-secondary">
-            <BsGripVertical className="me-2 fs-3" />
-            Week 1
-            <ModuleControlButtons />
-          </div>
-          <ul className="wd-lessons list-group rounded-0">
-            <li className="wd-lesson list-group-item p-3 ps-1">
-              <BsGripVertical className="me-2 fs-3" />
-              LEARNING OBJECTIVES
-              <LessonControlButtons />
-              <ul className="wd-content">
-                <li className="wd-content-item">
-                  <BsGripVertical className="me-2 fs-3" />
-                  Introduction to the course
-                  <LessonControlButtons />
-                </li>
-                <li className="wd-content-item">
-                  <BsGripVertical className="me-2 fs-3" />
-                  Learn what is Web Development
-                  <LessonControlButtons />
-                </li>
-              </ul>
-            </li>
+    const { cid } = useParams();
+    const modules = db.modules;
+    return (
+      <div id="wd-modules">
+        <div className="pb-5">
+          <ModuleControls/>
+        </div>
+        <div>
+          <ul id="wd-modules" className="list-group flex-fill rounded-0 pt-4">
+            {modules
+              .filter((module: any) => module.course === cid)
+              .map((module: any) => (
+              <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+                <div className="wd-title p-3 ps-2">
+                  <BsGripVertical className="me-2 fs-3"/>
+                  {module.name}
+                  <ModuleControlButtons />
+                </div>
+                {module.lessons && (
+                  <ul className="wd-lessons list-group rounded-0">
+                    {module.lessons.map((lessons: any) => (
+                    <li className="wd-lesson list-group-item p-3 ps-1 border-green">
+                      <BsGripVertical className="me-2 fs-3"/>
+                      {lessons.name}
+                      <LessonControlButtons />
+                    </li>
+                    ))}
+                  </ul>
+                )}
+              </li>
+            ))}
           </ul>
-        </li>
-        <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
-          <div className="wd-title p-3 ps-2 bg-secondary">
-            <BsGripVertical className="me-2 fs-3" />
-            Week 2
-            <ModuleControlButtons />
-          </div>
-          <ul className="wd-lessons list-group rounded-0">
-            <li className="wd-lesson list-group-item p-3 ps-1">
-              <BsGripVertical className="me-2 fs-3" />
-              LEARNING OBJECTIVES
-              <LessonControlButtons />
-              <ul className="wd-content">
-                <li className="wd-content-item">
-                  <BsGripVertical className="me-2 fs-3" />
-                  Basics of HTML
-                  <LessonControlButtons />
-                </li>
-                <li className="wd-content-item">
-                  <BsGripVertical className="me-2 fs-3" />
-                  Introduction to CSS
-                  <LessonControlButtons />
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-        {/* Add more weeks in a similar format */}
-      </ul>
-    </div>
-  );
-}
+        </div>
+      </div>
+  );}
--- src/Kanbas/Courses/Navigation/index.tsx
+++ src/Kanbas/Courses/Navigation/index.tsx
@@ -1,24 +1,16 @@
-import { useParams, useLocation, Link } from "react-router-dom";
-import "./index.css";
-
+import { Link, useLocation } from "react-router-dom";
+import "./index.css"
 export default function CoursesNavigation() {
-    const { cid } = useParams();
-    const { pathname } = useLocation();
-    const links = ["Home","Modules","Piazza","Zoom","Assignments","Quizzes","Grades",
-    ];
+    const { pathname } = useLocation()
+    const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades"];
     return (
-        <div id="wd-courses-navigation" className="list-group fs-5 rounded-0">
-            {links.map((link) => (
-                <a
-                    id="wd-course-home-link"
-                    href={`#/Kanbas/Courses/${cid}/${link}`}
-                    className={`list-group-item border border-0
-                    ${pathname.includes(link) ? "active" : "text-danger"}`}
-                >
-                    {" "}
-                    {link}{" "}
-                </a>
-            ))}
-        </div>
-    );
-}
+      <div id="wd-courses-navigation" className="list-group fs-5 rounded-0">
+        {links.map((link) => (
+          <Link key={link} to={link} className={`list-group-item text-danger border border-0
+              ${pathname.includes(link) ? "active" : ""}`}>
+            {link}
+          </Link>
+        ))}
+      </div>
+  );}
+  
