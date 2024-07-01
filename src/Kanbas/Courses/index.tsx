d874eb60c917674ea4905dd2790d6135afac7e82
Maxwell MacAvoy <maxwellmacavoy@Maxwells-MacBook-Air.local>
2024-07-01 08:22:06 +0200

    course adding and editing implemented

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -4,43 +4,42 @@ import Home from "./Home";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
 import Grades from "./Grades";
-import { courses } from "../Database";
-import { Navigate, Route, Routes, useParams, useLocation } from "react-router";
+import { useParams, useLocation, Navigate, Route, Routes } from "react-router";
 import { FaAlignJustify } from "react-icons/fa";
 
-export default function Courses() {
-    const { cid } = useParams();
-    const course = courses.find((course) => course._id === cid);
-    const { pathname } = useLocation();
+export default function Courses({ courses }: { courses: any[] }) {
+  const { cid } = useParams();
+  const course = courses.find((course) => course._id === cid);
+  const { pathname } = useLocation();
 
-    // Debugging logs
-    console.log("Course ID (cid):", cid);
-    console.log("Course data:", course);
-    console.log("Current pathname:", pathname);
+  // Debugging logs
+  console.log("Course ID (cid):", cid);
+  console.log("Course data:", course);
+  console.log("Current pathname:", pathname);
 
-    return (
-        <div id="wd-courses">
-            <h2 className="text-danger">
-                <FaAlignJustify className="me-4 fs-4 mb-1" />
-                {course && course.name} &gt; {pathname.split("/")[4]}
-            </h2>
-            <hr />
-            <br />
-            <div className="d-flex">
-                <div className="d-none d-md-block pe-5">
-                    <CoursesNavigation />
-                </div>
-                <div className="flex-fill">
-                    <Routes>
-                        <Route path="/" element={<Navigate to="Home" />} />
-                        <Route path="Home" element={<Home />} />
-                        <Route path="Modules" element={<Modules />} />
-                        <Route path="Assignments" element={<Assignments />} />
-                        <Route path="Assignments/:aid" element={<AssignmentEditor />} />
-                        <Route path="Grades" element={<Grades />} />
-                    </Routes>
-                </div>
-            </div>
+  return (
+    <div id="wd-courses">
+      <h2 className="text-danger">
+        <FaAlignJustify className="me-4 fs-4 mb-1" />
+        {course && course.name} &gt; {pathname.split("/")[4]}
+      </h2>
+      <hr />
+      <br />
+      <div className="d-flex">
+        <div className="d-none d-md-block pe-5">
+          <CoursesNavigation />
         </div>
-    );
+        <div className="flex-fill">
+          <Routes>
+            <Route path="/" element={<Navigate to="Home" />} />
+            <Route path="Home" element={<Home />} />
+            <Route path="Modules" element={<Modules />} />
+            <Route path="Assignments" element={<Assignments />} />
+            <Route path="Assignments/:aid" element={<AssignmentEditor />} />
+            <Route path="Grades" element={<Grades />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
 }
