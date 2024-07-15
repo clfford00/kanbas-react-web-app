aa28c970a889dbbfaa0ed2cc47adba82033d5787
Maxwell MacAvoy <maxwellmacavoy@Maxwells-MacBook-Air.local>
2024-07-15 07:32:20 -0400

    most of lab 5 working

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -6,6 +6,7 @@ export default function TOC() {
       <li><a id="wd-a2" href="#/Labs/Lab2">Lab 2</a></li>
       <li><a id="wd-a3" href="#/Labs/Lab3">Lab 3</a></li>
       <li><a id="wd-a4" href="#/Labs/Lab4">Lab 4</a></li>
+      <li><a id="wd-a5" href="#/Labs/Lab5">Lab 5</a></li>
       <li><a id="wd-k"  href="#/Kanbas">Kanbas</a></li>
 
     </ul>
